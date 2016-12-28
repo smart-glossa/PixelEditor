5ce577b42fe2b9ce8d2e88c3de77362a9af0e037
Sathish <sathishprs@smartglossa>
2016-12-28 17:04:31 +0530

    class file added

---
+++ pixeleditor/src/main/java/com/smartglossa/pixeleditor/DataBaseConstant.java
@@ -0,0 +1,12 @@
+package com.smartglossa.pixeleditor;
+
+public class DataBaseConstant {
+	public static String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+	public static String DATABASE_NAME = System.getProperty("pix.mysql.databaseName");// databasename=editor
+	public static String MYSQL_SERVER = System.getProperty("pix.mysql.server");;
+	public static String USERNAME = System.getProperty("pix.mysql.username");
+	public static String PASSWORD = System.getProperty("pix.mysql.password");
+	//public static String USERNAME = "root";   
+	//public static String PASSWORD = "root";
+
+}
